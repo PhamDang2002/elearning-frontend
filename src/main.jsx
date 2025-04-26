9d64c145c03c9c2ad29432c67b18c5b990208982
PhamDang2002 <phamdang559@gmail.com>
2025-04-26 08:34:49 +0700

    Thêm lazy

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,21 @@ import ModalProvider from "@context/ModalProvider";
 import { lazy } from "react";
 import { ThemeProvider } from "@mui/material";
 const HomePage = lazy(() => import("@pages/HomePage"));
+const About = lazy(() => import("@pages/about/About"));
+const Account = lazy(() => import("@pages/account/Account"));
+const Courses = lazy(() => import("@pages/courses/Courses"));
+const CourseDescription = lazy(
+  () => import("@pages/coursedescription/CourseDescription"),
+);
+const Payment = lazy(() => import("@components/Payment"));
+const Success = lazy(() => import("@components/Success"));
+const Cancel = lazy(() => import("@components/Cancel"));
+const Dashbord = lazy(() => import("@pages/dashbord/Dashbord"));
+const CourseStudy = lazy(() => import("@pages/coursestudy/CourseStudy"));
+const Lecture = lazy(() => import("@pages/lecture/Lecture"));
+const AdminDashbord = lazy(() => import("./admin/Dashboard/AdminDashbord"));
+const AdminCourses = lazy(() => import("./admin/Courses/AdminCourses"));
+const AdminUsers = lazy(() => import("./admin/Users/AdminUsers"));
 import theme from "./configs/muiConfig";
 import RegisterPage from "@pages/auth/RegisterPage";
 import AuthLayout from "@pages/auth/AuthLayout";
@@ -14,22 +29,9 @@ import OTPVerifyPage from "@pages/auth/OTPVerifyPage";
 import { Provider } from "react-redux";
 import { persistor, store } from "@redux/store";
 import ProtectedLayout from "@pages/ProtectedLayout";
-
 import { PersistGate } from "redux-persist/integration/react";
-import About from "@pages/about/About";
-import Account from "@pages/account/Account";
-import Courses from "@pages/courses/Courses";
-import CourseDescription from "@pages/coursedescription/CourseDescription";
-import Payment from "@components/Payment";
-import Success from "@components/Success";
-import Cancel from "@components/Cancel";
-import Dashbord from "@pages/dashbord/Dashbord";
-import CourseStudy from "@pages/coursestudy/CourseStudy";
-import Lecture from "@pages/lecture/Lecture";
 import Loading from "@components/Loading";
-import AdminDashbord from "./admin/Dashboard/AdminDashbord";
-import AdminCourses from "./admin/Courses/AdminCourses";
-import AdminUsers from "./admin/Users/AdminUsers";
+
 import ForgotPassword from "@pages/auth/ForgotPassword";
 import ResetPassword from "@pages/auth/ResetPassword";
 
