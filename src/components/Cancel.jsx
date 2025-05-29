baab06ba39a71e59a92027e2d35ece96339fb22c
PhamDang2002 <phamdang559@gmail.com>
2025-05-29 12:14:56 +0700

    new

--- src/components/Cancel.jsx
+++ src/components/Cancel.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 const Cancel = () => {
   const navigate = useNavigate();
   return (
-    <div>
+    <div className="!mt-28">
       Đã hủy thanh toán 231
       <Button onClick={() => navigate("/")}>Quay trở về trang chủ</Button>
     </div>
